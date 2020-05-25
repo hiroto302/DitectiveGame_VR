4a148ff98510a4542b28b8c55566f077b1a2f453
hiroto302 <i.am.a.hiroto@gmail.com>
2020-05-25 23:46:11 +0900

    FirstCommit

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+// オキュラスクエスト用 transform.TranslateによるPlayerの移動方法
+
+// Translate の 移動法でカクツク原因は, time.deltaTimeを利用すると起こるのが一つの原因であった
+// 現在の解決方法
+// 1. FPSが約40であるため、その乗数をかける
+//    ＝＞ 対象機種がオキュラスクエストであってもデバイスによる性能差が出る場合が問題である
+public class PlayerController : MonoBehaviour
+{
+    private float angleSpeed = 45.0f;
+    private float moveSpeed = 2.0f;
+    private float speedMultiplier = 0.02f;
+    [SerializeField]
+    Transform moveTarget = null;
+
+    // オキュラスクエストの入力値
+
+    float x, y;
+    Vector3 move = Vector3.zero;
+
+    void Reset()
+    {
+        if(!moveTarget)
+        {
+            moveTarget = GetComponentInChildren<OVRCameraRig>().transform.Find("TrackingSpace/CenterEyeAnchor");
+        }
+    }
+    void Update()
+    {
+        Debug.Log(Time.deltaTime.GetType());
+        // 左スティック入力 角度・旋回
+        Vector2 leftStick = OVRInput.Get(OVRInput.Axis2D.PrimaryThumbstick);
+        transform.Rotate(new Vector3(0, leftStick.x, 0) * angleSpeed * speedMultiplier);
+        // transform.Rotate(new Vector3(0, leftStick.x, 0) * angleSpeed * Time.deltaTime);
+
+        // 右スティック入力 方向・移動
+        Vector2 rightStick = OVRInput.Get(OVRInput.Axis2D.SecondaryThumbstick);
+        // x, y -1.0 ~ 1.0 の値
+        x = rightStick.x;
+        y = rightStick.y;
+        move = (x * moveTarget.right.normalized + y * moveTarget.forward.normalized ) * moveSpeed * speedMultiplier;
+        // move = (x * moveTarget.right.normalized + y * moveTarget.forward.normalized ) * moveSpeed * Time.deltaTime;
+        transform.Translate(move, Space.World);
+
+        // HMD内にデバッグ表示方法 表示するものは一つにすること
+        // OVRDebugConsole.Log(move.ToString("f5"));
+        // OVRDebugConsole.instance.AddMessage(move.ToString() + " : Time.deltaTime", Color.white);
+    }
+}
---
+++ Assets/Scripts/Player/PlayerControllerSecond.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Update と FixedUpdate で記述を分けてPlayerControllerの機能を記述
+
+
+public class PlayerControllerSecond : MonoBehaviour
+{
+    Rigidbody rb;
+    private float angleSpeed = 30.0f;
+    private float moveSpeed = 1.0f;
+    [SerializeField] GameObject moveTarget;
+    // オキュラスクエストの入力値変数
+
+    float x, y;
+    Vector3 rightStick;
+    Vector3 move = Vector3.zero;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+
+    void Update()
+    {
+        // 右スティック入力 方向
+        rightStick = OVRInput.Get(OVRInput.Axis2D.SecondaryThumbstick);
+        // 左スティック操作 角度
+        var leftStick = OVRInput.Get(OVRInput.Axis2D.PrimaryThumbstick);
+        Vector3 angleVector = new Vector3( 0, leftStick.x, 0);
+        transform.Rotate(0, angleVector.y * angleSpeed * Time.deltaTime, 0);
+    }
+
+    void FixedUpdate()
+    {
+            x = rightStick.x;
+            y = rightStick.y;
+            // x と z 軸のみの変更
+            // Vector3 direction = new Vector3( x , 0, y);
+            // Vector3 direction = new Vector3( x , 0, y);
+            // move = (direction.z * moveTarget.transform.forward.normalized + direction.x * moveTarget.transform.right.normalized) * moveSpeed * 0.001f;
+            // move =  rb.velocity;
+            var move = (y * moveTarget.transform.forward.normalized + x * moveTarget.transform.right.normalized) * 0.00001f;
+            rb.velocity = new Vector3( move.x, rb.velocity.y, move.z);
+    }
+}
