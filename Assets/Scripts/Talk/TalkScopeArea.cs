fc8eebb52fde67c4be3653c040262b3285e96002
hiroto302 <i.am.a.hiroto@gmail.com>
2020-06-16 21:40:04 +0900

    表示アイコンの基本機能追加

---
+++ Assets/Scripts/Talk/TalkScopeArea.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// キャラクターの任意の範囲にPlayerが近ずいた時、話すIconを表示するスクリプト
+public class TalkScopeArea : MonoBehaviour
+{
+    // アイコンを表示しているか判定
+    bool showIcon = false;
+    // 表示・非表示にする会話アイコン
+    [SerializeField]
+    GameObject talkIcon = null;
+    // プレイヤーの変数群
+    [SerializeField]
+    GameObject player = null;
+    Transform playerTarnsform;
+    float rotationSpeed = 1.0f;
+    void Reset()
+    {
+        // Talk_IconをTalkScopeAreaの子(0番目にセット)
+        talkIcon = transform.GetChild(0).gameObject;
+        // Playerの取得
+        player = GameObject.Find("Player");
+    }
+    void Awake()
+    {
+        // 初期は非表示
+        talkIcon.SetActive(false);
+        // Player 変数の初期化
+        playerTarnsform = player.GetComponent<Transform>();
+    }
+    void Start()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            showIcon = true;
+            // アイコンを表示する音
+        }
+    }
+    void OnTriggerStay(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            // アイコンを表示
+            talkIcon.SetActive(true);
+        }
+    }
+    void OnTriggerExit(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            showIcon = false;
+            // アイコンを非表示
+            talkIcon.SetActive(false);
+        }
+    }
+    void Update()
+    {
+        // アイコンを表示中のみ下記の処理を実行
+        if(showIcon == true)
+        {
+            // Playerの位置の方向にアイコンの向きわ合わせる
+            // form(第一引数)からto(第二引数)の間の角度を算出 yの位置をどちらかのオブジェクトに合わせる
+            // float f = Vector3.Angle(transform.forward, new Vector3(playerTarnsform.position.x, transform.position.y, playerTarnsform.position.z) - transform.position);
+            // LookRotation により、あるオブジェクトから見た別のオブジェクトの方向ベクトルを引数に渡すことで、そのベクトルに対するQuaternion型を取得
+            // Quaternion.Lerp により、現在位置(第一引数)から第二引数に向けて徐々に回転させていく
+            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(new Vector3(playerTarnsform.position.x, transform.position.y, playerTarnsform.position.z) - transform.position), rotationSpeed * Time.deltaTime);
+        }
+    }
+}
