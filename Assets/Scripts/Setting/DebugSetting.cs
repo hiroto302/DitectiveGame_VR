5797567939bf7cb58770ca9a850fee8f452d7625
hiroto302 <i.am.a.hiroto@gmail.com>
2021-03-29 19:52:43 +0900

    Update DebugSetting.cs

--- Assets/Scripts/Setting/DebugSetting.cs
+++ Assets/Scripts/Setting/DebugSetting.cs
@@ -5,10 +5,32 @@ using UnityEngine;
 // テストPlayが終了したらfalseにすること
 public class DebugSetting : MonoBehaviour
 {
-    void Start()
+    // OVRDebugConsole.Logメソッド実行時に、その内容を表示しているオブジェクト
+    [SerializeField]
+    GameObject OVRDebugConsoleObject = null;
+    [SerializeField]
+    GameObject OVRDebugConsoleOutputObject = null;
+
+    public bool testPlay = true;
+
+    void Reset()
     {
-        // Debug.Log で出力しているもののみ非表示
-        // Debug.unityLogger.logEnabled = false;
+        // オブジェクトの取得
+        OVRDebugConsoleObject = GameObject.Find("OVRDebugConsole");
+        OVRDebugConsoleOutputObject = GameObject.Find("OVRDebugConsoleOutput");
     }
 
+    void Start()
+    {
+        TestPlay(testPlay);
+    }
+
+    void TestPlay(bool testPlay)
+    {
+        // Debug.Log で出力しているものの表示・非表示
+        Debug.unityLogger.logEnabled = testPlay;
+        // OVRDebugConsole.Logの表示・非表示
+        OVRDebugConsoleObject.SetActive(testPlay);
+        OVRDebugConsoleOutputObject.SetActive(testPlay);
+    }
 }
